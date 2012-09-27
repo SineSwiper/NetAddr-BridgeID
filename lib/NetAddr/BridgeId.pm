c7e7bdf42e9e95641ae1cb459187a4ab9ef9ea72
Brendan Byrd <Perl@ResonatorSoft.org>
2012-09-27 18:43:15 -0400

    Initial commit

---
+++ lib/NetAddr/BridgeId.pm
@@ -0,0 +1,36 @@
+package NetAddr::BridgeId;
+
+# VERSION
+# ABSTRACT: ---insert abstract here---
+
+use sanity;
+
+### INSERT CODE HERE ###
+
+42;
+
+__END__
+
+=begin wikidoc
+
+= SYNOPSIS
+ 
+   # code
+ 
+= DESCRIPTION
+ 
+Insert description here...
+
+= CAVEATS
+
+Bad stuff...
+
+= SEE ALSO
+
+Other modules...
+
+= ACKNOWLEDGEMENTS
+
+Thanks and stuff...
+
+=end wikidoc
